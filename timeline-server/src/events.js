692ea1f464f317c3f38a7406d9c2657ed9b956ba
Hardy Scheel <hardy.scheel@gmail.com>
2020-09-10 10:05:32 +0200

    Authentication With JWT and Okta

--- timeline-server/src/events.js
+++ timeline-server/src/events.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 function createRouter(db) {
     const router = express.Router();
-    const owner = '';
+    const owner = req.user.email;
 
     // the routes are defined here
 
     // The POST route will only activate when the server receives an HTTP POST request.
     // Using the SQL INSERT statement, this code adds a new row to the events table.
     router.post('/event', (req, res, next) => {
+        const owner = req.user.email;
+
         db.query(
             'INSERT INTO events (owner, name, description, date) VALUES (?,?,?,?)',
             [owner, req.body.name, req.body.description, new Date(req.body.date)],
@@ -25,6 +27,8 @@ function createRouter(db) {
 
     // List the events of a single owner, the following route links an HTTP GET request to a MySQL SELECT statement. 
     router.get('/event', function (req, res, next) {
+        const owner = req.user.email;
+
         db.query(
             'SELECT id, name, description, date FROM events WHERE owner=? ORDER BY date LIMIT 10 OFFSET ?',
             [owner, 10 * (req.params.page || 0)],
@@ -41,6 +45,8 @@ function createRouter(db) {
 
     // Modify existing entries with PUT route, which links an HTTP PUT request to a MySQL UPDATE statement.
     router.put('/event/:id', function (req, res, next) {
+        const owner = req.user.email;
+
         db.query(
             'UPDATE events SET name=?, description=?, date=? WHERE id=? AND owner=?',
             [req.body.name, req.body.description, new Date(req.body.date), req.params.id, owner],
@@ -56,6 +62,8 @@ function createRouter(db) {
 
     // Delete an existing event. Do this through an HTTP DELETE request that issues a MySQL DELETE statement.
     router.delete('/event/:id', function (req, res, next) {
+        const owner = req.user.email;
+        
         db.query(
             'DELETE FROM events WHERE id=? AND owner=?',
             [req.params.id, owner],
--- timeline-server/src/index.js
+++ timeline-server/src/index.js
@@ -1,3 +1,5 @@
+const bearerToken = require('express-bearer-token');
+const oktaAuth = require('./auth');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -14,6 +16,8 @@ const port = process.env.PORT || 8080;
 const app = express()
     .use(cors())
     .use(bodyParser.json())
+    .use(bearerToken())
+    .use(oktaAuth)
     .use(events(connection));
 app.listen(port, () => {
     console.log(`Express server listening on port ${port}`);
