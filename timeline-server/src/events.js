fe643550f6abd0e9ede209249bb75573e1516b39
Hardy Scheel <hardy.scheel@gmail.com>
2020-09-10 09:33:57 +0200

    define routes of the REST API

--- timeline-server/src/events.js
+++ timeline-server/src/events.js
@@ -1,9 +1,75 @@
 const express = require('express');
+
 function createRouter(db) {
-  const router = express.Router();
-  const owner = '';
-  // the routes are defined here
-  return router;
+    const router = express.Router();
+    const owner = '';
+
+    // the routes are defined here
+
+    // The POST route will only activate when the server receives an HTTP POST request.
+    // Using the SQL INSERT statement, this code adds a new row to the events table.
+    router.post('/event', (req, res, next) => {
+        db.query(
+            'INSERT INTO events (owner, name, description, date) VALUES (?,?,?,?)',
+            [owner, req.body.name, req.body.description, new Date(req.body.date)],
+            (error) => {
+                if (error) {
+                    console.error(error);
+                    res.status(500).json({ status: 'error' });
+                } else {
+                    res.status(200).json({ status: 'ok' });
+                }
+            }
+        );
+    });
+
+    // List the events of a single owner, the following route links an HTTP GET request to a MySQL SELECT statement. 
+    router.get('/event', function (req, res, next) {
+        db.query(
+            'SELECT id, name, description, date FROM events WHERE owner=? ORDER BY date LIMIT 10 OFFSET ?',
+            [owner, 10 * (req.params.page || 0)],
+            (error, results) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({ status: 'error' });
+                } else {
+                    res.status(200).json(results);
+                }
+            }
+        );
+    });
+
+    // Modify existing entries with PUT route, which links an HTTP PUT request to a MySQL UPDATE statement.
+    router.put('/event/:id', function (req, res, next) {
+        db.query(
+            'UPDATE events SET name=?, description=?, date=? WHERE id=? AND owner=?',
+            [req.body.name, req.body.description, new Date(req.body.date), req.params.id, owner],
+            (error) => {
+                if (error) {
+                    res.status(500).json({ status: 'error' });
+                } else {
+                    res.status(200).json({ status: 'ok' });
+                }
+            }
+        );
+    });
+
+    // Delete an existing event. Do this through an HTTP DELETE request that issues a MySQL DELETE statement.
+    router.delete('/event/:id', function (req, res, next) {
+        db.query(
+            'DELETE FROM events WHERE id=? AND owner=?',
+            [req.params.id, owner],
+            (error) => {
+                if (error) {
+                    res.status(500).json({ status: 'error' });
+                } else {
+                    res.status(200).json({ status: 'ok' });
+                }
+            }
+        );
+    });
+
+    return router;
 }
 
 module.exports = createRouter;
--- timeline-server/src/index.js
+++ timeline-server/src/index.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const events = require('./events');
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'hscheel_timeline_user',
-  password : 'hscheel_timeline_user_password',
-  database : 'hscheel_timeline'
+    host: 'localhost',
+    user: 'hscheel_timeline_user',
+    password: 'hscheel_timeline_user_password',
+    database: 'hscheel_timeline'
 });
 connection.connect();
 const port = process.env.PORT || 8080;
 const app = express()
-  .use(cors())
-  .use(bodyParser.json())
-  .use(events(connection));
+    .use(cors())
+    .use(bodyParser.json())
+    .use(events(connection));
 app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+    console.log(`Express server listening on port ${port}`);
 });
