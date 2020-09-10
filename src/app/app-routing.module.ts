d7fd8a29a07637f9a7ba754d56a51e0f242c0aa7
Hardy Scheel <hardy.scheel@gmail.com>
2020-09-10 10:59:17 +0200

    add and implement home and timeline component and server service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'timeline',
+    component: TimelineComponent,
+    canActivate: [OktaAuthGuard]
+  },
+  { path: 'implicit/callback', component: OktaCallbackComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
